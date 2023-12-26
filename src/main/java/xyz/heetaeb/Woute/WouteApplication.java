70575f3efe444df3ef1145e9d5730a9d1c7178a4
121Mbp <nino09@naver.com>
2023-12-26 14:59:07 +0900

    [add] 세팅

---
+++ src/main/java/xyz/heetaeb/Woute/WouteApplication.java
@@ -0,0 +1,13 @@
+package xyz.heetaeb.Woute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WouteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/heetaeb/Woute/WouteApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.heetaeb.Woute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
