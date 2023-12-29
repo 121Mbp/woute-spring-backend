f07ba01611eb71053224cd704e6a9f8462496ea9
121Mbp <nino09@naver.com>
2023-12-29 10:52:55 +0900

    [add] feed api

--- src/main/java/xyz/heetaeb/Woute/domain/feed/controller/FeedController.java
+++ src/main/java/xyz/heetaeb/Woute/domain/feed/controller/FeedController.java
@@ -3,14 +3,19 @@ package xyz.heetaeb.Woute.domain.feed.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import xyz.heetaeb.Woute.domain.feed.dto.request.CourseRequest;
 import xyz.heetaeb.Woute.domain.feed.dto.request.FeedRequest;
 import xyz.heetaeb.Woute.domain.feed.dto.request.LikeRequest;
+import xyz.heetaeb.Woute.domain.feed.dto.request.TagsRequest;
 import xyz.heetaeb.Woute.domain.feed.dto.response.CourseResponse;
 import xyz.heetaeb.Woute.domain.feed.dto.response.FeedResponse;
 import xyz.heetaeb.Woute.domain.feed.service.FeedService;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,48 +25,65 @@ public class FeedController {
 
     @Operation(summary = "피드 목록")
     @GetMapping("/p")
-    public List<FeedResponse> getFeedList() {
+    public List<FeedResponse> getAllFeeds() {
         return feedService.feedList();
     }
 
     @Operation(summary = "피드 상세페이지")
     @GetMapping("/p/{id}")
-    public CourseResponse getCourseList(@PathVariable("id") Long id) {
+    public CourseResponse getCourseList(@PathVariable Long id) {
         return feedService.courseList(id);
+    }
+
+    @Operation(summary = "피드 이미지")
+    @GetMapping("/file/{uuid}")
+    public ResponseEntity<?> downImage(@PathVariable("uuid") String uuid) throws IOException {
+        byte[] downloadImage = feedService.downloadImageSystem(uuid);
+        if(downloadImage != null) {
+            return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("image/png"))
+                    .body(downloadImage);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 
     @Operation(summary = "피드 등록")
     @PostMapping("/p")
-    public ResponseEntity<String> insertFeedPost(@RequestBody FeedRequest request) {
+    public void insertFeedPost(
+            @RequestPart(value = "feed") FeedRequest requestFeed,
+            @RequestPart(value = "courses", required = false) List<CourseRequest> requestCourses,
+            @RequestPart(value = "tags", required = false) List<TagsRequest> requestTags,
+            @RequestPart(value = "attaches", required = false) List<MultipartFile> files
+    ) throws IOException {
         try {
-            feedService.insertFeed(request);
-            return ResponseEntity.ok("피드가 정상적으로 등록 되었습니다.");
+            feedService.insertFeed(requestFeed, requestCourses, requestTags, files);
         } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("피드가 등록되지 않았습니다. " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
     @Operation(summary = "피드 수정")
     @PutMapping("/p/{id}")
-    public void modifyFeedPost(@PathVariable("id") Long id, @RequestBody FeedRequest request) {
+    public void modifyFeedPost(@PathVariable Long id, @RequestBody FeedRequest request) {
         feedService.modifyFeed(id, request);
     }
 
     @Operation(summary = "피드 삭제")
     @DeleteMapping("/p/{id}")
-    public void deleteFeedPost(@PathVariable("id") Long id) {
+    public void deleteFeedPost(@PathVariable Long id) {
         feedService.deleteFeed(id);
     }
 
     @Operation(summary = "좋아요")
     @PutMapping("/p/{id}/like")
-    public void likeFeedPost(@PathVariable("id") Long id, @RequestBody LikeRequest request) {
+    public void likeFeedPost(@PathVariable Long id, @RequestBody LikeRequest request) {
         feedService.likeFeed(id, request);
     }
 
     @Operation(summary = "좋아요 취소")
     @PutMapping("/p/{id}/like/{likeId}")
-    public void unlikeFeedPost(@PathVariable("id") Long id, @PathVariable("likeId") Long likeId, @RequestBody LikeRequest request) {
+    public void unlikeFeedPost(@PathVariable Long id, @PathVariable Long likeId, @RequestBody LikeRequest request) {
         feedService.unlikeFeed(id, likeId, request);
     }
+
 }
---
+++ src/main/java/xyz/heetaeb/Woute/domain/feed/dto/response/TagsResponse.java
@@ -0,0 +1,13 @@
+package xyz.heetaeb.Woute.domain.feed.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TagsResponse {
+    private Long id;
+    private String words;
+}
