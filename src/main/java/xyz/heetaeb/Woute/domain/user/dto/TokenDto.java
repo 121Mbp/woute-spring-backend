84dca3182452137d6c183f72e6edec721cd5897d
veggie <cbk8094@gmail.com>
2024-01-04 12:23:33 +0900

    [add] user 추가

---
+++ src/main/java/xyz/heetaeb/Woute/domain/user/dto/TokenDto.java
@@ -0,0 +1,16 @@
+package xyz.heetaeb.Woute.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDto {
+	private String grantType;
+    private String accessToken;
+    private Long tokenExpiresIn;
+}
